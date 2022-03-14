c8f7b2d42573432e4dc013892a610f0b0ee047bb
hyuntae <hyuntae.kim@bolttech.io>
2022-03-14 13:29:13 +0900

    Removed: unnecessary code

--- src/server.js
+++ src/server.js
@@ -7,11 +7,6 @@ const buildAdminRouter = require('./admin.router');
 const app = express();
 const port = 3000;
 
-const Cat = mongoose.model('Cat', { name: String });
-
-const kitty = new Cat({ name: 'Zildjian' });
-kitty.save().then(() => console.log('meow'));
-
 const run = async () => {
   await mongoose.connect('mongodb://localhost:27017/test');
 
